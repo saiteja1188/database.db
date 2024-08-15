7b6fd5ab74ce0ce6ccdf23e9d09311e092f2e860
saiteja1188 <loveui19@gmail.com>
2024-08-15 12:12:00 +0530

    database.db 1

---
+++ mysql.sql
@@ -0,0 +1,6 @@
+-- CREATE TABLE user(id INT, username VARCHAR(250), password VARCHAR(250));
+
+-- INSERT INTO user(id, username, password)
+-- VALUES (1, 'saieja', '123456');
+
+-- SELECT * FROM user
